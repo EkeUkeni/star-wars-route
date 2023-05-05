8dae517e791aa48d42933b7a0444cb757f60d126
ekeukeni <ukenieke@gmail.com>
2023-05-05 22:07:03 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Films from './Components/Films'
+import Details404 from './Details404'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+        <Route path='/' element={<Films/>}/>
+
+        {/* * - wildcard */}
+        <Route path='*' element={<Details404 />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Films.js
@@ -0,0 +1,59 @@
+import { useEffect, useState} from "react"; 
+import MoreInfoBtn from "./MoreInfo";
+
+const Films = () => {
+    const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(()=>{
+
+    fetch(`https://swapi.dev/api/films`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`This is an HTTP Error: The status is ${response.status}`)
+                }
+                return response.json()
+            })
+            .then((actualData) => {
+                setData(actualData.results)
+                setError(null)
+            })
+            .catch((error) => {
+                console.log(error)
+                setError(error)
+                setData(null)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+
+      }, [])
+  return (
+    <>
+      <div style={{background:"black"}}>
+        <h1>Star Wars</h1>
+        {loading && <div>Data is loading. Please wait...</div>}
+        {error && <div>{`There is a problem fetching your data - ${error}`}</div>}
+        <ul>
+            {data && data.map((item) => {
+                      return (
+                          <li className='card' key={item.episode_id}>
+                              <div>
+                                <div className='text'>
+                                  <div className='title'><h2>{item.title}</h2></div>
+                                  <div className='date'><p>{item.release_date}</p></div>
+                                  <div className='redborder'><p>{item.opening_crawl.split('\n').slice(0, 10).join('\n')}...</p></div>
+                                  <div ><MoreInfoBtn text={'More Info'}/></div>
+                                </div> 
+                              </div>
+                          </li>
+                      )
+                  })}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Films
